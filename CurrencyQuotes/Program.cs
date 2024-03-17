345284252d92b37a277dc062118121f23e654ede
rizva <rizva@DESKTOP-L6EVAL5>
2024-03-18 03:03:53 +0500

    Добавьте файлы проекта.

---
+++ CurrencyQuotes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Text;
+
+namespace CurrencyQuotes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var currency =  Console.ReadLine();
+            Console.WriteLine("Текущая котировка: ");
+            Console.WriteLine(GetUSDRate(currency));
+        }
+
+        public static string GetUSDRate(string curr)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            string url = "http://www.cbr.ru/scripts/XML_daily.asp";
+            //XmlDocument xml_doc = new XmlDocument();
+            //xml_doc.Load(url);
+            DataSet ds = new DataSet();
+            ds.ReadXml(url);
+            DataTable currency = ds.Tables["Valute"];
+            foreach (DataRow row in currency.Rows)
+            {
+                if (row["CharCode"].ToString() == curr)//Ищу нужный код валюты
+                {
+                    return row["Value"].ToString(); //Возвращаю значение курсы валюты
+                }
+            }
+            return "";
+        }
+    }
+}
